5e15f3886125cdf4065bb4de7fb28f7f93a74345
edwinNieto <egnieto94@gmail.com>
2021-02-11 14:56:28 -0500

    password db

--- agenda/bdd.php
+++ agenda/bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=funtraining_db;charset=utf8', 'kqrfqf50m3f0', 'dxtw1K4*Sj2%');
+	$bdd = new PDO('mysql:host=localhost;dbname=funtraining_db;charset=utf8', 'kqrfqf50m3f0', 'krUHB#9(vO');
 }
 catch(Exception $e)
 {
