424200128578df59b4babf2348f695f1cec89321
edwinNieto <egnieto94@gmail.com>
2021-02-11 14:27:14 -0500

    first commit

---
+++ src/commons/common-scripts-caller.php
@@ -0,0 +1,4 @@
+<script src="/src/libs/jquery/jquery.min.js"></script>
+<script src="/src/libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="/src/js/general-script.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
---
+++ src/commons/common-styles-caller.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="/src/libs/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="/src/css/general-style.css">
