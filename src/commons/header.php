c393fcb1ab5606878c5b7799989e0c68219447f3
AFBGDeveloper <anresfb@gmail.com>
2021-02-14 05:57:26 -0500

    Header links added

--- src/commons/header.php
+++ src/commons/header.php
@@ -4,67 +4,33 @@
 			<path fill="v" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
 		</svg>
 	</div>
-
-
 	<div class="social-icons">
-
 		<a href="https://www.facebook.com/funtraininggym" target="_blank" rel="noopener noreferrer">
-
-			<img src="/src/img/fb.svg" alt="">			
-
+			<img src="/src/img/fb.svg" alt="">
 		</a>
-
-		
-
-		<a href="https://api.whatsapp.com/send?phone=573235220130&text=Quiero%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20plan%20N" target="_blank" rel="noopener noreferrer">
-
+		<a href="https://api.whatsapp.com/send?phone=573235220130&text=Quiero%20m%C3%A1s%20informaci%C3%B3n%20sobre%20el%20plan%20N"
+			target="_blank" rel="noopener noreferrer">
 			<img src="/src/img/wa.svg" alt="">
-
 		</a>
-
-		
-
 		<a href="https://www.instagram.com/funtraininggym/" target="_blank" rel="noopener noreferrer">
-
 			<img src="/src/img/ig.svg" alt="">
-
 		</a>
-
 	</div>
-
-
-
 	<ul id="menu">
-
 		<li>
-
-			<a href="/" class="item-menu active">HOME</a>
-
+			<a href="https://funtraining.net" class="item-menu">HOME</a>
 		</li>
-
 		<li>
-
-			<a href="/" class="item-menu">ENTRENADORES</a>
-
+			<a href="https://funtraining.net/entrenadores/" class="item-menu">ENTRENADORES</a>
 		</li>
-
 		<li>
-
-			<a href="/" class="item-menu">COMENTARIOS</a>
-
+			<a href="https://funtraining.net/comentarios/" class="item-menu">COMENTARIOS</a>
 		</li>
-
 		<li>
-
-			<a href="/" class="item-menu">SERVICIOS</a>
-
+			<a href="https://funtraining.net" class="item-menu">SERVICIOS</a>
 		</li>
-
 		<li>
-
-			<a href="/" class="item-menu">CONTACTENOS</a>
-
+			<a href="https://funtraining.net/contactenos/" class="item-menu">CONTACTENOS</a>
 		</li>
 	</ul>
-
 </header>
